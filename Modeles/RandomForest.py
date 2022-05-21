0ac81ce0d675ed03007963dcee0834171926395a
ThierryBissel <thierry.bissel@viacesi.fr>
2022-05-21 19:05:45 +0200

    RandomForest

---
+++ Modeles/RandomForest.py
@@ -0,0 +1,8 @@
+from sklearn.ensemble import RandomForestClassifier
+
+def RandomForestPredictor(X_train, y_train, X_test):
+    RandomForest = RandomForestClassifier(max_features=13, n_estimators=80)
+    RandomForest.fit(X_train, y_train)
+    y_pred = RandomForest.predict(X_test)
+    y_pred_proba = RandomForest.predict_proba(X_test)[:,1]
+    return y_pred, y_pred_proba
