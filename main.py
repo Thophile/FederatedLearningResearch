4baed4e515763fe0ed7da8e67a962722022c521a
Tarru <valentin.reinbold@viacesi.fr>
2022-05-24 11:30:27 +0200

    New Data

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import EvaluatePrediction as PredictionEvaluator
 import Modeles.RandomForest as RandomForestPredictor
 import Modeles.GradientDecent as Grad
 
-pandaDf = DW.LoadAll()
+pandaDf = DW.LoadOne(3)
 
 pandaDf.info()
 
 
-x_train, y_train, x_test, y_test = DW.onSplit(pandaDf)
+#x_train, y_train, x_test, y_test = DW.onSplit(pandaDf)
 
 
 #y_pred, y_pred_proba = RandomForestPredictor.RandomForestPredictor(x_train, y_train, x_test)
-y_pred, y_pred_proba = Grad.GradientDescentPredictor(x_train, y_train, x_test)
-PredictionEvaluator.EvaluatePrediction(y_test, y_pred, y_pred_proba)
+#y_pred, y_pred_proba = Grad.GradientDescentPredictor(x_train, y_train, x_test)
+#PredictionEvaluator.EvaluatePrediction(y_test, y_pred, y_pred_proba)
