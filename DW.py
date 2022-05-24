d6a98f91e6a45e0e9ae957db2dd7de8f9d68a976
ThierryBissel <thierry.bissel@viacesi.fr>
2022-05-24 15:25:16 +0200

    uwu

--- DW.py
+++ DW.py
@@ -1,46 +1,52 @@
 from email import header
+from matplotlib.pyplot import axis
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.utils import shuffle
 from sklearn import preprocessing
 
 
-def loadData(filePath, cols):
+def loadData(filePath):
     return pd.read_csv(filePath,
         engine='c',
         index_col=False,
-        usecols=cols, 
         quoting=3,
         header=0,
-        delimiter=";")
+        delimiter=",")
 
 
 def LoadAll():
-    fulldf = LoadOne(62)
-    for i in ['67','83','85','87','91']:
+    fulldf = LoadOne(3)
+    for i in ['4','5','6','7']:
         fulldf.append(LoadOne(i))
     return fulldf
 
 def LoadOne(number):
     number = str(number)
-    batDf = loadData(".\data\\batiments\\batiment_"+number+".csv", ["mtedle2019_elec_conso_tot", "bnb_id", "adedpe202006_logtype_min_classe_ener_ges", "adedpe202006_logtype_ratio_ges_conso", "adedpe202006_logtype_periode_construction", "adedpe202006_min_conso_ener", "adedpe202006_min_estim_ges", "adedpe202006_max_conso_ener", "adedpe202006_max_estim_ges", "anarnc202012_nb_log", "anarnc202012_nb_lot_tertiaire", "anarnc202012_nb_lot_tot", "mtedle2019_elec_conso_res_par_pdl_res", "mtedle2019_elec_nb_pdl_tot", "mtedle2019_gaz_conso_res_par_pdl_res", "mtedle2019_gaz_nb_pdl_tot", "adedpe202006_logtype_baie_fs", "adedpe202006_logtype_baie_mat", "adedpe202006_logtype_baie_orientation", "adedpe202006_logtype_baie_remplissage", "adedpe202006_logtype_baie_type_vitrage", "adedpe202006_logtype_ch_gen_lib_appoint", "adedpe202006_logtype_ch_gen_lib_princ", "adedpe202006_logtype_ch_is_solaire", "adedpe202006_logtype_ch_type_inst", "adedpe202006_logtype_ecs_gen_lib_princ", "adedpe202006_logtype_inertie", "adedpe202006_logtype_mur_ep_mat_ext", "adedpe202006_logtype_mur_mat_ext", "adedpe202006_logtype_mur_pos_isol_ext", "adedpe202006_logtype_pb_mat", "adedpe202006_logtype_pb_pos_isol", "adedpe202006_logtype_ph_pos_isol", "adedpe202006_logtype_presence_balcon", "adedpe202006_logtype_presence_climatisation", "adedpe202006_logtype_type_batiment", "adedpe202006_logtype_type_ventilation", "adedpe202006_nb_classe_ene_a", "adedpe202006_nb_classe_ene_b", "adedpe202006_nb_classe_ene_c", "adedpe202006_nb_classe_ene_d", "adedpe202006_nb_classe_ene_e", "adedpe202006_nb_classe_ene_f", "adedpe202006_nb_classe_ene_g", "adedpe202006_nb_classe_ene_nc", "cerffo2020_mat_mur_txt", "cerffo2020_mat_toit_txt", "igntop202103_bat_hauteur"])
-    batDf.info()
-    print("Bat OK")
-    relAddBatDf= loadData(".\data\\relAddBats\\rel_adresse_batiment_opendata_"+number+".csv", ["bnb_id", "etaban202111_id"])
-    relAddBatDf.info()
-    print("Rel OK")
-    adressDf= loadData(".\data\\adresses\\adresse_"+number+".csv", ["etaban202111_id", "etaban202111_latitude", "etaban202111_longitude"])
-    adressDf.info()
-    print("Adresse OK")
-    temp = pd.merge(batDf, relAddBatDf, how="left", on=["bnb_id"])
-    temp.info()
-    print("Temp1 OK")
-    singleDF = pd.merge(temp, adressDf, how="left", on=["etaban202111_id"])
-    singleDF.info()
-    print("singleDF OK")
-    return singleDF
+    print('--------------------------------------------Loading csv '+ number +'---------------------------------------')
+    FloorDf = loadData(".\data\\2018Floor"+number+".csv")
+    FloorDf.info()
+    print("Floor 2018 OK")
+    FloorDf.append(loadData(".\data\\2019Floor"+number+".csv"))
+    FloorDf.info()
+    print("Floor 2019OK")
+    return FloorDf
 
+def Wrangling(DF):
+    print('--------------------------------------------Wrangling---------------------------------------')
+    for col in DF:
+        if(DF.dtypes[col]=="float"):
+            tempAverage = DF[col].mean()
+            DF[col] = np.where(np.isnan(DF[col]), tempAverage, DF[col])
+
+    DF["averageDegre"] = DF[['z1_S1(degC)', 'z2_S1(degC)', 'z4_S1(degC)', 'z5_S1(degC)']].mean(axis=1)
+    DF["averageHumidity"] = DF[['z1_S1(RH%)', 'z2_S1(RH%)', 'z4_S1(RH%)', 'z5_S1(RH%)']].mean(axis=1)
+    DF["averageLux"] = DF[['z1_S1(lux)', 'z2_S1(lux)', 'z4_S1(lux)', 'z5_S1(lux)']].mean(axis=1)
+
+    return DF
+
+
+ 
 
 
 def onSplit(pandaDF):
@@ -48,43 +54,21 @@ def onSplit(pandaDF):
 
     print("---------------------------------------------Split--------------------------------------")
 
-    pandaDF.drop("bnb_id", axis=1, inplace=True)
-    #pandaDF.drop("etaban202111_id", axis=1, inplace=True)
+    #split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 
-    pandaDFD = pandaDF.copy()
+    #for train_index, test_index in split.split(pandaDF, pandaDF[["averageDegre", "averageHumidity", "averageLux"]]):
+    #    strat_train_set = pandaDF.loc[train_index]
+    #    strat_test_set = pandaDF.loc[test_index]
+
+    #strat_train_label = strat_train_set[["averageDegre", "averageHumidity", "averageLux"]].copy()
+    #strat_train_set = strat_train_set.drop(["averageDegre", "averageHumidity", "averageLux"], axis=1)
     
-    le = preprocessing.LabelEncoder()
-
-    pandaDF_2 = pandaDF.apply(le.fit_transform)
-
-      
-    pandaDF_2["mtedle2019_elec_conso_tot"] = pd.cut(pandaDF_2["mtedle2019_elec_conso_tot"], 32, labels=np.arange(32))
-
-    enc = preprocessing.OneHotEncoder()
-    enc.fit(pandaDF_2)
-
-    transformed = enc.transform(pandaDF_2)
-
-    oheDF = pd.DataFrame(transformed)
-
-    for categorie in pandaDFD.columns:
-
-        pandaDFD[categorie] = pd.concat([pandaDF[categorie], oheDF], axis=1).drop(categorie, axis=1)
-
-
-    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-
-    for train_index, test_index in split.split(pandaDF_2, pandaDF_2["mtedle2019_elec_conso_tot"]):
-        strat_train_set = pandaDF_2.loc[train_index]
-        strat_test_set = pandaDF_2.loc[test_index]
-
-    strat_train_label = strat_train_set["mtedle2019_elec_conso_tot"].copy()
-    strat_train_set = strat_train_set.drop('mtedle2019_elec_conso_tot', axis=1)
+    #strat_test_label = strat_test_set[["averageDegre", "averageHumidity", "averageLux"]].copy()
+    #strat_test_set = strat_test_set.drop(["averageDegre", "averageHumidity", "averageLux"], axis=1)
     
-    strat_test_label = strat_test_set["mtedle2019_elec_conso_tot"].copy()
-    strat_test_set = strat_test_set.drop('mtedle2019_elec_conso_tot', axis=1)
-
-    return strat_train_set, strat_train_label, strat_test_set, strat_test_label
+    pandaDF = shuffle(pandaDF)
+    
+    return pandaDF.drop(["averageDegre", "averageHumidity", "averageLux"], axis=1).to_numpy(), pandaDF[["averageDegre", "averageHumidity", "averageLux"]].copy().to_numpy()
 
 def x_y_split(pandaDF, limit=False):
     print("---------------------------------------------Split--------------------------------------")
@@ -114,4 +98,5 @@ def x_y_split(pandaDF, limit=False):
 
     if limit:
         pandaDF_2 = pandaDF_2[:limit]
+    print(pandaDF_2.mtedle2019_elec_conso_tot[100])
     return pandaDF_2.drop('mtedle2019_elec_conso_tot', axis=1).to_numpy(), pandaDF_2["mtedle2019_elec_conso_tot"].copy().to_numpy()
--- main.py
+++ main.py
@@ -4,14 +4,18 @@ import EvaluatePrediction as PredictionEvaluator
 import Modeles.RandomForest as RandomForestPredictor
 import Modeles.GradientDecent as Grad
 
-pandaDf = DW.LoadAll()
+pandaDf = DW.LoadOne(3)
+pandaDf = DW.Wrangling(pandaDf)
 
 pandaDf.info()
+print(pandaDf.head())
 
 
-x_train, y_train, x_test, y_test = DW.onSplit(pandaDf)
+X, y = DW.onSplit(pandaDf)
+
+
 
 
 #y_pred, y_pred_proba = RandomForestPredictor.RandomForestPredictor(x_train, y_train, x_test)
-y_pred, y_pred_proba = Grad.GradientDescentPredictor(x_train, y_train, x_test)
-PredictionEvaluator.EvaluatePrediction(y_test, y_pred, y_pred_proba)
+#y_pred, y_pred_proba = Grad.GradientDescentPredictor(x_train, y_train, x_test)
+#PredictionEvaluator.EvaluatePrediction(y_test, y_pred, y_pred_proba)
